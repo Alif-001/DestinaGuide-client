d23a24a64c4808d55f70224ea7eaafc1aeeeab18
Md. Omor Faruk Alif <87005321+Alif-001@users.noreply.github.com>
2025-03-06 20:37:27 +0600

    Implement Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar/>
       <h1 className="text-red-500">DestinaGuide</h1>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import Routes from "./routes/Routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Routes} />
   </StrictMode>
 );
