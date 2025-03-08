1a803e2d2bf3e7a38b6b6790671c63778ed7bd89
Md. Omor Faruk Alif <87005321+Alif-001@users.noreply.github.com>
2025-03-08 20:29:54 +0600

    Implement NotFound page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import NotFound from '../pages/NotFound';
 
 const Routes = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <NotFound/>
   },
 ]);
 
