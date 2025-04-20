936a36231daa4d4e9e4d4c573360bbcf47ea3760
Md. Omor Faruk Alif <87005321+Alif-001@users.noreply.github.com>
2025-04-20 23:56:15 +0600

    Fix Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-neutral text-neutral-content fixed bottom-0 left-0 w-full z-50">
+    <footer className="footer footer-center p-4 bg-neutral text-neutral-content  bottom-0 left-0 w-full ">
       <div>
         <h2 className="font-bold text-xl">Destinaguide</h2>
         <p className="text-sm">Your ultimate tourism guide.</p>
